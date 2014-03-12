956912b4f58ac2a5739c5b1e368b5b338c69943b
akasima <akasima@nate.com>
2014-03-12 20:00:01 +0900

    modify document_content_change

--- document_content_change/document_content_change.addon.php
+++ document_content_change/document_content_change.addon.php
@@ -1,37 +1,32 @@
 <?php
-/* Copyright (C) NAVER <http://www.navercorp.com> */
-
 /**
- * @file exchange_content.addon.php
- * @brief Addon for change content matched pattern's
- * @author [NAVER](http://www.navercorp.com) (<developers@xpressengine.com)*/
+  * @file document_content_change.addon.php
+  * @biref Depending on the type of posts to change the contents of the set.
+  * @author NAVER (developers@xpressengine.com)
+  */
 
 if(!defined('__XE__'))
 	exit();
 
-$document_srl = Context::get('document_srl');
-if($called_position=="after_module_proc" && $this->module=="board")
+if($called_position == 'after_module_proc' && $this->module=="board")
 {
-	$document_srl = Context::get('document_srl');
-	if(!$document_srl)
-		return false;
-
 	$cur_act = Context::get('act');
 	if($cur_act != "" && $cur_act != "dispBoardContent")
-		return false;
+		return;
 	
-	$oDocument = Context::get('oDocument');	// set from documentView::dispBoardContentView()
+	$document_srl = Context::get('document_srl');
+	if(!$document_srl)
+		return;
+
+	$oDocument = Context::get('oDocument');
 	if(!$oDocument)
-		return false;
+		return;
 
 	if($oDocument->document_srl != $document_srl)
-		return false;
+		return;
 	
-	$pattern = array();
-	$pattern['phone_number'] = "/(0[0-9]{1,2})-([0-9]{3,4})-([0-9]{4})/i";
-
-	$replace['phone_number'] = "$1-$2-****";
+	$pattern = "/(0[0-9]{1,2})-([0-9]{3,4})-([0-9]{4})/i";
+	$replace = "$1-$2-****";
 
 	$oDocument->variables['content'] = preg_replace($pattern, $replace, $oDocument->variables['content']);
-
 }
